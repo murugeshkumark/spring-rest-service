0cbf5d8307c83589773591657b60ae3c40227b43
Murugesh Kumar K <master4uall@gmail.com>
2018-05-11 20:57:03 -0400

    LDAP Authentication integration

--- src/main/java/com/master4uall/spring/rest/config/SecurityConfig.java
+++ src/main/java/com/master4uall/spring/rest/config/SecurityConfig.java
@@ -2,33 +2,41 @@ package com.master4uall.spring.rest.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
     protected void configure(AuthenticationManagerBuilder auth)
             throws Exception {
-            auth
-                    .inMemoryAuthentication()
-                    .passwordEncoder(org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance())
-                    .withUser("user1")
-                    .password("secret1")
-                    .roles("USER")
-                    .and()
-                    .withUser("admin1")
-                    .password("secret1")
-                    .roles("USER", "ADMIN");
+        auth
+                .ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=springframework,dc=org")
+                .and()
+                .passwordCompare()
+                .passwordEncoder(new LdapShaPasswordEncoder())
+                .passwordAttribute("userPassword");
     }
 
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
             http
                 .httpBasic()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/**")
-                .hasRole("USER").and()
+                .anyRequest()
+                .fullyAuthenticated()
+                 .and()
                 .csrf()
                 .disable()
                 .headers()
